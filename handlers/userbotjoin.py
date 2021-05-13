73bfa1eaaf317f499b682552b8afd11fa38ee972
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-13 22:54:50 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -39,7 +39,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "OxyXMusic"
+        user.first_name =  "MahakalXMusic"
 
     try:
         await USER.join_chat(invitelink)
